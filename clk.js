a34ef8687f4e0bd56b1c85af70338b3c54a8a94f
Cosine07 <dhruvrameshbhaipatel2@gmail.com>
2023-06-15 23:38:23 +0530

    first commit

---
+++ clk.js
@@ -0,0 +1,19 @@
+setInterval(() => { 
+    d=new Date(); 
+    htime=d.getHours();
+    mtime=d.getMinutes();
+    stime=d.getSeconds(); 
+    
+    console.log(htime);
+    console.log(mtime);
+    console.log(stime); 
+    
+    hrotation=htime*30 + mtime/2;
+    mrotation=6*mtime;
+    srotation=6*stime;
+      
+    hour.style.transform=`rotate(${hrotation}deg)`; 
+    minute.style.transform=`rotate(${mrotation}deg)`;
+    second.style.transform=`rotate(${srotation}deg)`; 
+}, 1000);   
+ 
